a50a6763600823c03c957d553df176496a82399d
lucas-catto <lucascattosilva2024@gmail.com>
2024-12-23 21:49:03 -0300

    example of crud with chaining functions in php.

---
+++ database.sql
@@ -0,0 +1,8 @@
+
+CREATE DATABASE functions;
+USE functions;
+
+CREATE TABLE users (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(60) NOT NULL
+);
--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
 
+/*
 // each method returns an anonymous object
 function crud() {
 
@@ -30,3 +31,99 @@ function crud() {
 }
 
 crud()->create()->read()->update()->delete();
+*/
+
+function message(string $message = "") {
+
+    if (!isset($message)) {
+        die('message undefined.');
+    }
+
+    echo 'Message: ' . $message . "\n";
+}
+
+function crud() {
+    
+    return new class
+    {
+        private $pdo;
+
+        public function __construct()
+        {
+            $this->pdo = new PDO("mysql:host=localhost;dbname=functions", "root", "");
+        }
+
+        public function create(string $name = '')
+        {
+            if ($name == '') { // !isset($name) || $name == ''
+                die("\$name is undefined.\n");
+            }
+            
+            $query = $this->pdo->prepare('INSERT INTO users VALUES (null, ?)');
+            $query->execute([$name]);
+
+            message('create ' . $name);
+            
+            return $this;
+        }
+
+        public function read(int $id = null, bool $show = true)
+        {    
+            if ($id == null) {
+                $query = $this->pdo->prepare('SELECT * FROM users');
+                $query->execute();
+            } else {
+                $query = $this->pdo->prepare('SELECT * FROM users WHERE id = ?');
+                $query->execute([$id]);
+            }
+
+            if ($show) {
+
+                $items = $query->fetchAll(PDO::FETCH_OBJ);
+    
+                foreach ($items as $item) {
+                    echo $item->id . " - ".$item->name . "\n";
+                }
+            }
+
+            return $this;
+        }
+
+        public function update(int $id = null, string $name = '')
+        {    
+            if ($id == null || $name == '') {
+                die('id or name undefined.');
+            }
+
+            $query = $this->pdo->prepare('UPDATE users SET name = ? WHERE id = ?');
+            $query->execute([$name, $id]);
+
+            message('update ' . $name);
+
+            return $this;
+        }
+
+        public function delete(int $id = null)
+        {
+            if ($id == null) {
+                die('id undefined.');
+            }
+
+            //message("delete: ".$this->read($id));
+            
+            $query = $this->pdo->prepare('DELETE FROM users WHERE id = ?');
+            $query->execute([$id]);
+
+            return $this;
+        }
+    };
+}
+
+// crud()->create('lucas')->read(6)->update(6, 'php')->delete(6);
+// crud()->delete(1);
+// crud()->update(1, 'laravel')->read();
+// crud()->create('lucas')->read(1);
+
+crud()->create();
+
+crud()->read();
